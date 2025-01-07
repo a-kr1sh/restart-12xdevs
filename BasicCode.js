2a4149be8d58ca9426f3a6e8df178822d545ea8c
krixech <krixech12@gmail.com>
2025-01-07 15:08:49 +0530

    first js code

---
+++ BasicCode.js
@@ -0,0 +1,10 @@
+// Syntax of Javascript
+
+// 1. Variables
+let name = "John";     // Variable that can be reassigned
+const age = 30;        // Constant variable that cannot be reassigned
+var isStudent = true;  // Older way to declare variables, function
+
+console.log(name)
+console.log(age)
+console.log(isStudent)
